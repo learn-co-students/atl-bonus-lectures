0fe50699fd4e6a50213ec3f1c180a4e7a724e4ae
kingcons <brit@kingcons.io>
2019-05-29 13:35:00 -0400

    GildedRose is really just a factory for making other kinds of items.
    We will have to make a minor change to the tests here because we can't get .new to return an instance  of another class in ruby.

--- testify/lib/gilded_rose.rb
+++ testify/lib/gilded_rose.rb
@@ -1,56 +1,18 @@
+require_relative './item'
+require_relative './brie'
+require_relative './backstage'
+require_relative './conjured'
+require_relative './normal'
+
 class GildedRose
-  attr_accessor :name, :sell_in, :quality
-
-  def initialize(name, sell_in, quality)
-    @name = name
-    @sell_in = sell_in
-    @quality = quality
-  end
-
-  def tick
-    if name == 'Aged Brie'
-      return brie_tick
-    elsif name == 'Backstage passes to a TAFKAL80ETC concert'
-      return backstage_tick
-    elsif name == 'Sulfuras, Hand of Ragnaros'
-      return sulfuras_tick
-    elsif name == "Conjured Mana Cake"
-      return conjured_tick
-    else
-      normal_tick
-    end
-  end
-
-  def normal_tick
-    @sell_in -= 1
-    @quality -= 1
-    @quality -= 1 if @sell_in <= 0
-    @quality = 0 if @quality < 0
-  end
-
-  def brie_tick
-    @sell_in -= 1
-    @quality += 1
-    @quality += 1 if @sell_in <= 0
-    @quality = 50 if @quality > 50
-  end
-
-  def backstage_tick
-    @sell_in -= 1
-    @quality += 1
-    @quality += 1 if @sell_in <= 10
-    @quality += 1 if @sell_in <= 5
-    @quality = 50 if @quality > 50
-    @quality = 0 if @sell_in < 0
-  end
-
-  def sulfuras_tick
-  end
-
-  def conjured_tick
-    @sell_in -= 1
-    @quality -= 2
-    @quality -= 2 if @sell_in <= 0
-    @quality = 0 if @quality < 0
+  def self.for(name, sell_in, quality)
+    items = {
+      'Aged Brie' => Brie,
+      'Backstage passes to a TAFKAL80ETC concert' => Backstage,
+      'Sulfuras, Hand of Ragnaros' => Item,
+      'Conjured Mana Cake' => Conjured
+    }
+    item_class = items[name] || Normal
+    item_class.new(name, sell_in, quality)
   end
 end
