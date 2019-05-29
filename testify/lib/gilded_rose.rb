6b1ece45bdafb5109e7c0f541b7b559a71d19ec5
kingcons <brit@kingcons.io>
2019-05-29 12:44:40 -0400

    Refactor sulfuras_tick and backstage_tick.

--- testify/lib/gilded_rose.rb
+++ testify/lib/gilded_rose.rb
@@ -12,6 +12,10 @@ class GildedRose
       return example_tick
     elsif name == 'Aged Brie'
       return brie_tick
+    elsif name == 'Backstage passes to a TAFKAL80ETC concert'
+      return backstage_tick
+    elsif name == 'Sulfuras, Hand of Ragnaros'
+      return sulfuras_tick
     end
     if @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
       if @quality > 0
@@ -70,5 +74,17 @@ class GildedRose
     @quality += 1
     @quality += 1 if @sell_in <= 0
     @quality = 50 if @quality > 50
+  end
+
+  def backstage_tick
+    @sell_in -= 1
+    @quality += 1
+    @quality += 1 if @sell_in <= 10
+    @quality += 1 if @sell_in <= 5
+    @quality = 50 if @quality > 50
+    @quality = 0 if @sell_in < 0
+  end
+
+  def sulfuras_tick
   end
 end
