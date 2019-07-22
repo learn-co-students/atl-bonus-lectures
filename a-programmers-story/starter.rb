289be7270715bf7c03db9429ca09f7ceea1d7856
kingcons <brit@kingcons.io>
2019-07-21 23:56:21 -0400

    Add intro algo lecture.

---
+++ a-programmers-story/starter.rb
@@ -0,0 +1,19 @@
+def dedupe(items)
+  ## Input: An array of items of unknown type.
+  ## Output: An array with any _sequential duplicates_ removed.
+  ## Example: dedupe([1,2,3,3,3,4,5,3]) -> [1,2,3,4,5,3]
+end
+
+def pair_sums(numbers, target)
+  ## Input: An array of numbers and a target to reach.
+  ## Output: An array of pairs of numbers that sum to the target.
+  ## Example: pair_sums([4,9,8,27,22], 31) -> [[4, 31], [31,4], [9,22], [22,9]]
+  ## Bonus: No duplicates, i.e. [[4,31], [9, 22]]
+end
+
+def mul(a, b)
+  ## Input: Two numbers to be multiplied, a and b.
+  ## Output: The product of a and b.
+  ## Example: mul(4, 5) -> 20
+  ## NOTE: Do not use the built-in multiplication operator.
+end
